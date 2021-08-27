ac7b3e0cf9c986a3d4eb16eb47a1bdfd3cf2f905
Gogs <gogs@fake.local>
2021-08-27 03:01:52 -0400

    Changed the package to support different distributions

--- R/NGS.R
+++ R/NGS.R
@@ -1,109 +1,94 @@
-g.c <- function(x, S_tau, rf, T2M, vol){
+g.c <- function(x, outer, df){
   g.x <- 0
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
-  for(i in 1:length(S_tau)){
-    g.x <- g.x + dnorm(x, mu[i], sig)^2
+  for(i in 1:length(outer)){
+    g.x <- g.x + df(x, outer[i])^2
   }
   return(sqrt(g.x))
 }
 
-g.star <- function(x, S_tau, rf, T2M, vol, h){
-  ST <- exp(x)
-  return(abs(h(ST)) * g.c(x, S_tau, rf, T2M, vol))
+g.star <- function(x, outer, df, h){
+  return(abs(h(x)) * g.c(x, outer, df))
 }
 
-f.beta <- function(x, beta, S_tau, rf, T2M, vol){
+f.beta <- function(x, beta, outer, df){
   f.x <- 0
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
-  for(i in 1:length(S_tau)){
-    f.x <- f.x + beta[i] * dnorm(x, mu[i], sig)
+  for(i in 1:length(outer)){
+    f.x <- f.x + beta[i] * df(x, outer[i])
   }
   return(f.x)
 }
 
-A_R.b <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, M){
-  N_Out <- length(S_tau)
+A_R.b <- function(outer, df, rf, N_In = 1e2, h, M){
+  N_Out <- length(outer)
 
-  sig <- vol * sqrt(T2M)
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-
-  # Sampling Mixture Normal r.v.
+  # Sampling Mixture r.v.
   U <- runif(N_Out*N_In*N_Out)
   U <- ceiling(U*N_Out)
-  mixed <- rnorm(N_Out*N_In*N_Out, mu[U], sig)
+  mixed <- rf(N_Out*N_In*N_Out, outer[U])
 
   # Computing L1, L2 sum
   beta <- rep(1/N_Out, N_Out)
-  f.x <- f.beta(mixed, beta, S_tau, rf, T2M, vol) * N_Out
-  g.x <- g.c(mixed, S_tau, rf, T2M, vol)
-  ST <- exp(mixed)
-  h.x <- h(ST)
+  f.x <- f.beta(mixed, beta, outer, df) * N_Out
+  g.x <- g.c(mixed, outer, df)
 
   # Acceptance and Rejection
   Z <- runif(N_Out*N_In*N_Out)
-  a <- (Z < ((abs(h.x)*g.x) / (M*f.x)))
+  ll <- h(mixed) * g.x
+  a <- (Z < (abs(ll)/(M*f.x)))
   rv <- mixed[a]
   sampled <- rv[1:(N_Out*N_In)]
+  c <- (N_Out*N_In) / (M * sum(a))
 
-  result <- list("c" = (N_Out*N_In) / (M * sum(a)), "sampled" = sampled)
+  result <- list("c" = c, "sampled" = sampled, "ll" = c * ll[a][1:(N_Out*N_In)])
   return(result)
 }
 
-A_R.c <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2){
-  N_Out <- length(S_tau)
+A_R.c <- function(outer, df, rf, N_In = 1e2){
+  N_Out <- length(outer)
 
-  sig <- vol * sqrt(T2M)
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-
-  # Sampling Mixture Normal r.v.
+  # Sampling Mixture r.v.
   U <- runif(N_Out*N_In*N_Out)
   U <- ceiling(U*N_Out)
-  mixed <- rnorm(N_Out*N_In*N_Out, mu[U], sig)
+  mixed <- rf(N_Out*N_In*N_Out, outer[U])
 
   # Computing L1, L2 sum
   # Computing L1, L2 sum
   beta <- rep(1/N_Out, N_Out)
-  f.x <- f.beta(mixed, beta, S_tau, rf, T2M, vol) * N_Out
-  g.x <- g.c(mixed, S_tau, rf, T2M, vol)
+  f.x <- f.beta(mixed, beta, outer, df) * N_Out
+  g.x <- g.c(mixed, outer, df)
 
   # Acceptance and Rejection
   Z <- runif(N_Out*N_In*N_Out)
   a <- (Z < (g.x/f.x))
   rv <- mixed[a]
   sampled <- rv[1:(N_Out*N_In)]
+  c <- (N_Out*N_In) / sum(a)
 
-  result <- list("c" = (N_Out*N_In) / sum(a), "sampled" = sampled)
+  result <- list("c" = c, "sampled" = sampled, "ll" = c * g.x[a][1:(N_Out*N_In)])
   return(result)
 }
 
-ls.fitting <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h){
-  N_Out <- length(S_tau)
+ls.fitting <- function(outer, df, rf, N_In = 1e2, h){
+  N_Out <- length(outer)
 
-  sig <- vol * sqrt(T2M)
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-
-  # Sampling Mixture Normal r.v.
+  # Sampling Mixture r.v.
   U <- rep((1:N_Out), N_In)
-  mixed <- rnorm(N_Out*N_In, mu[U], sig)
+  mixed <- rf(N_Out*N_In, outer[U])
 
   # Computing L1, L2 sum
   f.x <- 0
   g.x <- 0
   X <- NULL
-  for(i in 1:length(S_tau)){
-    l.i <- dnorm(mixed, mu[i], sig)
+  for(i in 1:length(outer)){
+    l.i <- df(mixed, outer[i])
     f.x <- f.x + l.i
     g.x <- g.x + l.i^2
     X <- cbind(X, l.i)
   }
   g.x <- sqrt(g.x)
-  ST <- exp(mixed)
-  h.x <- h(ST)
 
   #Fitting beta
-  Y = (abs(h.x)*g.x)
+  Y = (abs(h(mixed))*g.x)
   soln <- nnls(X, Y)
   beta <- soln$x
   beta <- beta / sum(beta)
@@ -112,22 +97,19 @@ ls.fitting <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h
   return(result)
 }
 
-ls.c.fitting <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2){
-  N_Out <- length(S_tau)
+ls.c.fitting <- function(outer, df, rf, N_In = 1e2){
+  N_Out <- length(outer)
 
-  sig <- vol * sqrt(T2M)
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-
-  # Sampling Mixture Normal r.v.
+  # Sampling Mixture r.v.
   U <- rep((1:N_Out), N_In)
-  mixed <- rnorm(N_Out*N_In, mu[U], sig)
+  mixed <- rf(N_Out*N_In, outer[U])
 
   # Computing L1, L2 sum
   f.x <- 0
   g.x <- 0
   X <- NULL
-  for(i in 1:length(S_tau)){
-    l.i <- dnorm(mixed, mu[i], sig)
+  for(i in 1:length(outer)){
+    l.i <- df(mixed, outer[i])
     f.x <- f.x + l.i
     g.x <- g.x + l.i^2
     X <- cbind(X, l.i)
@@ -144,63 +126,23 @@ ls.c.fitting <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2)
   return(result)
 }
 
-NGS_MIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, S0 = 100, T = 1, h, sn = FALSE){
-  N_Out <- length(S_tau)
+NGS_MLR <- function(outer, df, rf, N_In = 1e2, h, sn = FALSE){
+  N_Out <- length(outer)
 
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
-
-  # Sampling Marginal Normal r.v.
-  sampled <- rnorm(N_Out*N_In, log(S0) + (rf-0.5*vol^2)*T, vol * sqrt(T))
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
-
-  # Calculate likelihoods
-  llh.g <- dnorm(sampled, log(S0) + (rf-0.5*vol^2)*T, vol * sqrt(T))
-
-  # Calculate option payoffs
-  C_tau_MIS <- NULL
-  if(sn){
-    for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
-      lr <- llh.f / llh.g
-      C_tau <- C_tau_sample * lr
-      C_tau_MIS <- c(C_tau_MIS, mean(C_tau) / mean(lr))
-    }
-  }
-  else{
-    for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
-      lr <- llh.f / llh.g
-      C_tau <- C_tau_sample * lr
-      C_tau_MIS <- c(C_tau_MIS, mean(C_tau))
-    }
-  }
-
-  return(C_tau_MIS)
-}
-
-NGS_MLR <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, sn = FALSE){
-  N_Out <- length(S_tau)
-
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
-
-  # Sampling Mixture Normal r.v.
+  # Sampling Mixture r.v.
   U <- rep((1:N_Out), N_In)
-  sampled <- rnorm(N_Out*N_In, mu[U], sig)
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
+  sampled <- rf(N_Out*N_In, outer[U])
+  C_tau_sample = h(sampled)
 
   # Calculate likelihoods
   beta <- rep(1/N_Out, N_Out)
-  llh.g <- f.beta(sampled, beta, S_tau, rf, T2M, vol)
+  llh.g <- f.beta(sampled, beta, outer, df)
 
   # Calculate option payoffs
   C_tau_MLR <- NULL
   if(sn){
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_MLR <- c(C_tau_MLR, mean(C_tau) / mean(lr))
@@ -208,7 +150,7 @@ NGS_MLR <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, s
   }
   else{
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_MLR <- c(C_tau_MLR, mean(C_tau))
@@ -218,26 +160,22 @@ NGS_MLR <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, s
   return(C_tau_MLR)
 }
 
-NGS_OIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, M, sn = FALSE){
-  N_Out <- length(S_tau)
+NGS_OIS <- function(outer, df, rf, N_In = 1e2, h, M, sn = FALSE){
+  N_Out <- length(outer)
 
   # Sampling OIS r.v.
-  A.R <- A_R.b(S_tau, rf, T2M, vol, N_In, h, M)
+  A.R <- A_R.b(outer, df, rf, N_In, h, M)
   sampled <- A.R$sampled
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
+  C_tau_sample = h(sampled)
 
   # Calculate likelihoods
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
-  c <- A.R$c
-  llh.g <- c * g.star(sampled, S_tau, rf, T2M, vol, h)
+  llh.g <- A.R$ll
 
   # Calculate option payoffs
   C_tau_OIS <- NULL
   if(sn){
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_OIS <- c(C_tau_OIS, mean(C_tau) / mean(lr))
@@ -245,7 +183,7 @@ NGS_OIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, M
   }
   else{
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_OIS <- c(C_tau_OIS, mean(C_tau))
@@ -255,26 +193,22 @@ NGS_OIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, M
   return(C_tau_OIS)
 }
 
-NGS_CIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, sn = FALSE){
-  N_Out <- length(S_tau)
+NGS_CIS <- function(outer, df, rf, N_In = 1e2, h, sn = FALSE){
+  N_Out <- length(outer)
 
   # Sampling CIS r.v.
-  A.R <- A_R.c(S_tau, rf, T2M, vol, N_In)
+  A.R <- A_R.c(outer, df, rf, N_In)
   sampled <- A.R$sampled
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
+  C_tau_sample = h(sampled)
 
   # Calculate likelihoods
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
-  c <- A.R$c
-  llh.g <- c * g.c(sampled, S_tau, rf, T2M, vol)
+  llh.g <- A.R$ll
 
   # Calculate option payoffs
   C_tau_CIS <- NULL
   if(sn){
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_CIS <- c(C_tau_CIS, mean(C_tau) / mean(lr))
@@ -282,7 +216,7 @@ NGS_CIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, s
   }
   else{
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_CIS <- c(C_tau_CIS, mean(C_tau))
@@ -292,32 +226,28 @@ NGS_CIS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, h, s
   return(C_tau_CIS)
 }
 
-NGS_LS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, p, h, sn = FALSE){
-  N_Out <- length(S_tau)
-
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
+NGS_LS <- function(outer, df, rf, N_In = 1e2, p, h, sn = FALSE){
+  N_Out <- length(outer)
 
   # Estimate beta and sample
-  result <- ls.fitting(S_tau, rf, T2M, vol, N_In, h)
+  result <- ls.fitting(outer, df, rf, N_In, h)
   beta <- result$beta
 
   sampled <- NULL
   n <- as.integer(beta * N_Out * as.integer(p*N_In))
   for (i in 1:N_Out){
-    sampled <- c(sampled, rnorm(n[i], mu[i], sig))
+    sampled <- c(sampled, rf(n[i], outer[i]))
   }
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
+  C_tau_sample = h(sampled)
 
   # Calculate likelihoods
-  llh.g <- f.beta(sampled, beta, S_tau, rf, T2M, vol)
+  llh.g <- f.beta(sampled, beta, outer, df)
 
   # Calculate option payoffs
   C_tau_QP <- NULL
   if(sn){
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_QP <- c(C_tau_QP, mean(C_tau) / mean(lr))
@@ -325,7 +255,7 @@ NGS_LS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, p, h,
   }
   else{
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_QP <- c(C_tau_QP, mean(C_tau))
@@ -335,32 +265,28 @@ NGS_LS <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, p, h,
   return(C_tau_QP)
 }
 
-NGS_LS.c <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, p, h, sn = FALSE){
-  N_Out <- length(S_tau)
-
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
+NGS_LS.c <- function(outer, df, rf, N_In = 1e2, p, h, sn = FALSE){
+  N_Out <- length(outer)
 
   # Estimate beta and sample
-  result <- ls.c.fitting(S_tau, rf, T2M, vol, N_In)
+  result <- ls.c.fitting(outer, df, rf, N_In)
   beta <- result$beta
 
   sampled <- NULL
   n <- as.integer(beta * N_Out * as.integer(p*N_In))
   for (i in 1:N_Out){
-    sampled <- c(sampled, rnorm(n[i], mu[i], sig))
+    sampled <- c(sampled, rf(n[i], outer[i]))
   }
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
+  C_tau_sample = h(sampled)
 
   # Calculate likelihoods
-  llh.g <- f.beta(sampled, beta, S_tau, rf, T2M, vol)
+  llh.g <- f.beta(sampled, beta, outer, df)
 
   # Calculate option payoffs
   C_tau_QP <- NULL
   if(sn){
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_QP <- c(C_tau_QP, mean(C_tau) / mean(lr))
@@ -368,7 +294,7 @@ NGS_LS.c <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, p, 
   }
   else{
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_QP <- c(C_tau_QP, mean(C_tau))
@@ -378,29 +304,25 @@ NGS_LS.c <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, p, 
   return(C_tau_QP)
 }
 
-NGS_LS.fb <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, beta, p, h, sn = FALSE){
-  N_Out <- length(S_tau)
-
-  mu <- log(S_tau) + (rf-0.5*vol^2)*T2M
-  sig <- vol * sqrt(T2M)
+NGS_LS.fb <- function(outer, df, rf, N_In = 1e2, beta, p, h, sn = FALSE){
+  N_Out <- length(outer)
 
   # Sample from fixed beta
   sampled <- NULL
   n <- as.integer(beta * N_Out * as.integer(p*N_In))
   for (i in 1:N_Out){
-    sampled <- c(sampled, rnorm(n[i], mu[i], sig))
+    sampled <- c(sampled, rf(n[i], outer[i]))
   }
-  ST = exp(sampled)
-  C_tau_sample = h(ST)
+  C_tau_sample = h(sampled)
 
   # Calculate likelihoods
-  llh.g <- f.beta(sampled, beta, S_tau, rf, T2M, vol)
+  llh.g <- f.beta(sampled, beta, outer, df)
 
   # Calculate option payoffs
   C_tau_QP <- NULL
   if(sn){
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_QP <- c(C_tau_QP, mean(C_tau) / mean(lr))
@@ -408,7 +330,7 @@ NGS_LS.fb <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, be
   }
   else{
     for (i in 1:N_Out){
-      llh.f <- dnorm(sampled, mu[i], sig)
+      llh.f <- df(sampled, outer[i])
       lr <- llh.f / llh.g
       C_tau <- C_tau_sample * lr
       C_tau_QP <- c(C_tau_QP, mean(C_tau))
@@ -416,4 +338,36 @@ NGS_LS.fb <- function(S_tau, rf = 5e-2, T2M = 11/12, vol = 30e-2, N_In = 1e2, be
   }
 
   return(C_tau_QP)
+}
+
+NGS_MIS <- function(outer, df, rf, dg, rg, N_In = 1e2, h, sn = FALSE){
+  N_Out <- length(outer)
+
+  # Sampling Marginal r.v.
+  sampled <- rg(N_Out*N_In)
+  C_tau_sample = h(sampled)
+
+  # Calculate likelihoods
+  llh.g <- dg(sampled)
+
+  # Calculate option payoffs
+  C_tau_MIS <- NULL
+  if(sn){
+    for (i in 1:N_Out){
+      llh.f <- df(sampled, outer[i])
+      lr <- llh.f / llh.g
+      C_tau <- C_tau_sample * lr
+      C_tau_MIS <- c(C_tau_MIS, mean(C_tau) / mean(lr))
+    }
+  }
+  else{
+    for (i in 1:N_Out){
+      llh.f <- df(sampled, outer[i])
+      lr <- llh.f / llh.g
+      C_tau <- C_tau_sample * lr
+      C_tau_MIS <- c(C_tau_MIS, mean(C_tau))
+    }
+  }
+
+  return(C_tau_MIS)
 }
